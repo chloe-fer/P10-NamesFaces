2d2a935bcfa35b56e9495715fd665ff5e5176f96
chloe-fer <62980379+chloe-fer@users.noreply.github.com>
2021-08-29 21:06:13 +0800

    Day 42 - implement UIImagePickerController

---
+++ Project-10/Person.swift
@@ -0,0 +1,12 @@
+//
+//  Person.swift
+//  Project-10
+//
+//  Created by Chloe Fermanis on 29/8/21.
+//
+
+import UIKit
+
+class Person: NSObject {
+
+}
---
+++ Project-10/PersonCell.swift
@@ -0,0 +1,12 @@
+//
+//  PersonCell.swift
+//  Project-10
+//
+//  Created by Chloe Fermanis on 28/8/21.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    
+}
